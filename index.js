ab15e1d181477ac461d02439a2a7021acf7ef076
leandrovelosos <leandrovdsantos96@gmail.com>
2023-09-29 12:22:42 -0300

    menu de opcoes

--- index.js
+++ index.js
@@ -2,4 +2,25 @@ import inquirer from 'inquirer';
 import chalk from 'chalk';
 import fs from 'fs';
 
-console.log("Accounts")
+function operation(){
+    inquirer.prompt([{
+        type:'list',
+        name: 'action',
+        message: 'O que você deseja fazer?',
+        choices:[
+            'Cria conta',
+            'Consultar saldo',
+            'Depositar',
+            'Sacar',
+            'Sair'
+        ]
+
+    }]).then((result) => {
+        const action = result['action']
+        console.log(action)
+    }).catch((err) => {
+        console.log(err)
+    });
+}
+
+operation()
